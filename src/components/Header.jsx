ef626fbb781242620db3b3b6fc2e1ae577e6dfdc
Dmitry Savelev <dsavelev1703@yandex.ru>
2023-05-22 16:12:10 +0700

    wrote readme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   }
 
   return (
-    <div className="Header">
+    <div className="header">
       <div className="logo"></div>
       <div className="header-column">
         <div className="socials">
